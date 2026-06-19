79ccce4e6a90cb1964b69785fd7a53bf8b50b6c4
SundayMorning-Dev <contatosundaydev@gmail.com>
2026-06-18 23:39:07 -0300

    versão 1.0

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+
+
 const navbarToggle = document.querySelector('.navbar-toggle');
 const navbarMenu = document.querySelector('.navbar-menu');
 
@@ -5,3 +7,17 @@ navbarToggle.addEventListener('click', () => {
     navbarToggle.classList.toggle('active');
     navbarMenu.classList.toggle('active');
 });
+
+  // Seleciona todos os selects do seu formulário
+  document.querySelectorAll('.select-wrapper select').forEach(select => {
+    
+    // Quando o usuário clica para abrir (ganha foco)
+    select.addEventListener('focus', () => {
+      select.parentElement.parentElement.classList.add('expandir-campo');
+    });
+
+    // Quando o usuário escolhe a opção ou clica fora (perde o foco)
+    select.addEventListener('blur', () => {
+      select.parentElement.parentElement.classList.remove('expandir-campo');
+    });
+  });
